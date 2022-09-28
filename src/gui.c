cc0c3c7468d291fef3398bea662d8b6be32fd3a0
g.martineaux <g.martineaux@student.isartdigital.com>
2022-09-28 11:45:52 +0200

    Initial Commit

---
+++ src/gui.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <playground.h>
+
+#include "gui.h"
+
+int im_print_text(int x, int y, const char* text)
+{
+
+}
+
+int im_print(int x, int y, const char* format, ...)
+{
+
+}
+
+
+bool im_button(int x, int y, const char* text)
+{
+
+}
